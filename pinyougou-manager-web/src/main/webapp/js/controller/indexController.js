8bf462f0d1a635cd48d0a40aeeab0bf1dd5ad773
tianhaolin1991 <tianhaolin1991@126.com>
2019-03-08 10:29:48 +0800

    v1.3 shop-web

---
+++ pinyougou-manager-web/src/main/webapp/js/controller/indexController.js
@@ -0,0 +1,12 @@
+app.controller('indexController',function($scope,loginService){
+
+	$scope.getUsername = function(){
+		loginService.getUsername().success(
+			function(response){
+				$scope.username = response.username;
+			
+			}
+		);
+		
+	}
+})
